dbfbbb52becf0245a41a3939441cabd03abd01f2
jason <jason@omnistory.net>
2022-10-21 20:45:32 +0900

    Add video conference

--- video-conference/conference.js
+++ video-conference/conference.js
@@ -10,6 +10,7 @@ window.onload = async function(){
 				break;
 			case "BROADCASTING_EVENT":
 				log.insertAdjacentHTML('beforeend', `<p>Join: ${evt.user_id}</p>`);
+				omnitalk?.subscribe(evt["publish_idx"]);
 				break;
 			case "ONAIR_EVENT":
 				if (evt.track_type == 1) log.insertAdjacentHTML('beforeend', `<p>Audio On</p>`);
@@ -27,9 +28,9 @@ window.onload = async function(){
 
 	regiBtn.addEventListener("click", async function() {
 		let roomName = document.getElementById('roomName').value;
-		let roomId = await omnitalk.createRoom("audioroom", roomName);
-		let roomlist = await omnitalk.roomList("audioroom");
-		log.insertAdjacentHTML('beforeend', `<p>Audio RoomId: ${roomId}</p>`);
+		let roomId = await omnitalk.createRoom("videoroom", roomName);
+		let roomlist = await omnitalk.roomList("videoroom");
+		log.insertAdjacentHTML('beforeend', `<p>Video RoomId: ${roomId}</p>`);
 
 		roomlist.map((item, index) => {
 			log.insertAdjacentHTML('beforeend', `<p>Roomlist-${index}: ${item.subject}, ${item.room_id}</p>`);
@@ -39,13 +40,16 @@ window.onload = async function(){
 	});
 
 	joinBtn.addEventListener("click", async function() {
+		document.getElementById('videoDisplay').style.display = "block";
+
 		let roomId = document.getElementById('roomId').value;
 		let result = await omnitalk.joinRoom(roomId);
 		let partilist = await omnitalk.partiList(roomId);
-		await omnitalk.publish("audiocall", false);
+		await omnitalk.publish("videocall", false);
 
 		partilist.map((item, index) => {
 			log.insertAdjacentHTML('beforeend', `<p>Participant-${index}: ${item.user_id}</p>`);
+			omnitalk?.subscribe(item.publish_idx);
 		})
 	});
 }
